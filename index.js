b3db845f4d5ac65150f135ced76942584bbe4a4e
Rachael Sewell <rachmari@github.com>
2021-01-19 04:46:53 -0800

    check pull request author (#18)
    * check pull request author
    
    * Update index.js
    
    Co-authored-by: James M. Greene <JamesMGreene@github.com>
    
    Co-authored-by: James M. Greene <JamesMGreene@github.com>

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const main = async () => {
   const token = core.getInput('github-token')
   const branch = core.getInput('branch')
   const base = core.getInput('base')
+  const author = core.getInput('author')
 
   const query = {
     ...context.repo,
@@ -23,7 +24,9 @@ const main = async () => {
   const octokit = new GitHub(token)
 
   const res = await octokit.pulls.list(query)
-  const pr = res.data.length && res.data[0]
+  const pr = author ?
+    res.data.length && res.data.filter(pr => pr.user.login === author)[0] :
+    res.data.length && res.data[0]
 
   core.debug(`pr: ${JSON.stringify(pr, null, 2)}`)
   core.setOutput('number', pr ? pr.number : '')
